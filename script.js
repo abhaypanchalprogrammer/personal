50b8cf6b1a16ae9d385c4e65cb91a600cdb1a383
abhaypanchalprogrammer <abhaypanchal2525@gmail.com>
2024-11-12 20:06:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menulinks");
+  const icon = document.querySelector(".hi");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+function nextPagee() {
+  window.open(
+    "https://www.instagram.com/abhay_dance14?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
+  );
+}
+function nextPage() {
+  window.open("/assets/CV.pdf");
+}
